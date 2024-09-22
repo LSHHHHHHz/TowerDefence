16ab55c0390e940aef9b8c73d429366002c4f23d
Hoon <opunho118@naver.com>
2024-09-23 00:20:33 +0900

    ShopPopup 초기화 순서 변경(버튼이 처음에 눌리지 않았었음)

--- Assets/Scripts/Manager/PopupManager.cs
+++ Assets/Scripts/Manager/PopupManager.cs
@@ -8,6 +8,11 @@ public class PopupManager : MonoBehaviour
 {
     public ShopPopup shopPopup;
     public AudioSettingUI audioSettingUI;
+    private void Start()
+    {
+        shopPopup.gameObject.SetActive(false);
+        audioSettingUI.gameObject.SetActive(false);
+    }
     public void OpenShopPopup()
     {
         shopPopup.gameObject.SetActive(true);
